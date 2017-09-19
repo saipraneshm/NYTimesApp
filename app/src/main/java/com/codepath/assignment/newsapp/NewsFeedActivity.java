e3e139884c3d79443e566b73e5abca96f4fd4a44
sai pranesh mukkala <saipranesh.mukkala@sjsu.edu>
2017-09-19 15:21:24 -0700

    Added Databinding support and other project dependencies

--- app/src/main/java/com/codepath/assignment/newsapp/NewsFeedActivity.java
+++ app/src/main/java/com/codepath/assignment/newsapp/NewsFeedActivity.java
@@ -1,13 +1,14 @@
 package com.codepath.assignment.newsapp;
 
+import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class NewsFeedActivity extends AppCompatActivity {
+public class NewsFeedActivity extends SingleFragmentActivity {
+
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_news_feed);
+    protected Fragment createFragment() {
+        return new NewsFeedFragment();
     }
 }
---
+++ app/src/main/java/com/codepath/assignment/newsapp/SingleFragmentActivity.java
@@ -0,0 +1,39 @@
+package com.codepath.assignment.newsapp;
+
+import android.databinding.DataBindingUtil;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v7.app.AppCompatActivity;
+
+import com.codepath.assignment.newsapp.databinding.LayoutFragmentContainerBinding;
+
+/**
+ * Created by saip92 on 9/19/2017.
+ */
+
+public abstract class SingleFragmentActivity extends AppCompatActivity {
+
+
+    private LayoutFragmentContainerBinding binding;
+
+    protected abstract Fragment createFragment();
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        binding = DataBindingUtil.setContentView(this,R.layout.layout_fragment_container);
+
+        FragmentManager fm = getSupportFragmentManager();
+        Fragment fragment = fm.findFragmentById(R.id.fragment_container);
+
+        if(fragment == null){
+            fragment = createFragment();
+            fm.beginTransaction().replace(R.id.fragment_container, fragment).commit();
+        }
+
+
+    }
+}
