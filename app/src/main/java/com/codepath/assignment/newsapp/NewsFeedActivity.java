7ce3de2995f71d4115eb9446e39767f7e85b1979
sai pranesh mukkala <saipranesh.mukkala@sjsu.edu>
2017-09-19 14:08:08 -0700

    Initial Commit

---
+++ app/src/main/java/com/codepath/assignment/newsapp/NewsFeedActivity.java
@@ -0,0 +1,13 @@
+package com.codepath.assignment.newsapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class NewsFeedActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_news_feed);
+    }
+}
